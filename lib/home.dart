cb54e8c72c6329e4a2a6b7063ca0d74b3739932e
jydv402 <jydv402@gmail.com>
2024-05-13 11:49:55 +0530

    Sample

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('This is a sample'),
+        ],
+      )),
+    );
+  }
+}
